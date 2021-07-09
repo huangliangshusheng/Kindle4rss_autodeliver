1e0a78015d2c7355fd43d2d8052f09102e2077dd
huangliangshush <huangliangshusheng@gmail.com>
2021-07-09 08:32:36 +0000

    update main.py

--- main.py
+++ main.py
@@ -2,28 +2,30 @@ import time
 import argparse
 import requests
 
+url = "https://kindle4rss.com"
+
 def push(emailAddress, password):
     session = requests.session()
     data = {
-        'email_address': emailAddress,
-        'password': password,
-        'persistent': 'True'
+        "email_address": emailAddress,
+        "password": password,
+        "persistent": "True"
     }  # login data
     headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
-        'referer': 'https://kindle4rss.com/feeds/explore/?bundles=zh'
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
+        "referer": f"{url}/feeds/explore/?bundles=zh"
     }
     session.headers = headers
-    session.post('https://kindle4rss.com/login/', data=data)
+    session.post(f"{url}/login/", data=data)
     time.sleep(5)
-    r = session.post('https://kindle4rss.com/send_now/')
+    r = session.post(f"{url}/send_now/")
     return r.status_code
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Automatic Script for Kindle4rss")
-    parser.add_argument('-u', '--user', type=str, required=True, help='User Name, Mostly E-mail Address')
-    parser.add_argument('-p', '--password', type=str, required=True, help='Login Password')
+    parser.add_argument("-u", "--user", type=str, required=True, help="User Name, Mostly E-mail Address")
+    parser.add_argument("-p", "--password", type=str, required=True, help="Login Password")
     args = parser.parse_args()
 
     code = push(args.user, args.password)
